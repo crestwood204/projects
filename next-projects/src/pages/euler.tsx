6cf18927bae90d0fde1fd7dea91f2eb555aae81a
Andrew Ong <andrew@fanhouse.app>
2023-04-23 11:26:39 -0400

    Feat: problem 8

--- next-projects/src/pages/euler.tsx
+++ next-projects/src/pages/euler.tsx
@@ -6,6 +6,7 @@ import problem4 from "@/components/EulerProject/problems/problem4";
 import problem5 from "@/components/EulerProject/problems/problem5";
 import problem6 from "@/components/EulerProject/problems/problem6";
 import problem7 from "@/components/EulerProject/problems/problem7";
+import problem8 from "@/components/EulerProject/problems/problem8";
 import { useState } from "react";
 import styled from "styled-components";
 
@@ -22,6 +23,7 @@ const EulerProject = () => {
         <Problem problemNumber={5} runProblem={problem5} autoRun={runAll} />
         <Problem problemNumber={6} runProblem={problem6} autoRun={runAll} />
         <Problem problemNumber={7} runProblem={problem7} autoRun={runAll} />
+        <Problem problemNumber={8} runProblem={problem8} autoRun={runAll} />
       </ProblemContainer>
       <button onClick={() => setRunAll(true)}>Run all</button>
     </PageContainer>
